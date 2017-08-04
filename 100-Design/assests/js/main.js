a5f6a0619bfe5534eacd57fa68743568306b68d1
Sakib Hossain <Sakibhossain.nstu@gmail.com>
2017-08-05 00:56:38 +0600

    Update Cwl-Carousel

---
+++ 100-Design/assests/js/main.js
@@ -0,0 +1,15 @@
+ jQuery(document).ready(function($){
+
+	  $('.team-carousel').owlCarousel({
+                items:3,
+                margin: 30,
+                loop:true,
+                nav:true,
+                navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+                autoplay:true,
+                autoplayTimeout:1500,
+
+                
+            });
+
+ });
