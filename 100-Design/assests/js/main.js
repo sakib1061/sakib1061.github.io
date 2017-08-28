b3557604a574bb377afd4b53f386e36fe08c80b3
Sakib Hossain <Sakibhossain.nstu@gmail.com>
2017-08-28 14:44:24 +0600

    3 CLasses

--- 100-Design/assests/js/main.js
+++ 100-Design/assests/js/main.js
@@ -7,9 +7,16 @@
                 nav:true,
                 navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
                 autoplay:true,
-                autoplayTimeout:1500,
-
-                
+                autoplayTimeout:1500,                
             });
+	  $('.Class8-carousel').owlCarousel({
+	  	items: 4,
+	  	margin: 30,
+	  	loop:true,
+	  	nav:true,
+	  	navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	  	autoplay:true,
+	  	autoplayTimeout:1000,
+	  });
 
  });
