5298b1a756c6aac1be9b7cfbf64e5f717b0dac89
Sakib Hossain <Sakibhossain.nstu@gmail.com>
2017-09-04 01:51:58 +0600

    Class10

---
+++ 100-Design/assests/js/Class10.js
@@ -0,0 +1,12 @@
+ jQuery(document).ready(function($){
+
+	 $('.Class10-carousel').owlCarousel({
+	  	items: 1,
+	  	loop:true,
+	  	nav:true,
+	  	navText:["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+	  	autoplay:true,
+	  	autoplayTimeout:1000,
+	  });
+
+ });
